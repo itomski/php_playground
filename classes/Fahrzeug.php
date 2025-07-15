46e96abf873b84f2f2360ce84a915693b9a8d65b
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2025-07-15 11:51:04 +0200

    OOP

--- classes/Fahrzeug.php
+++ classes/Fahrzeug.php
@@ -2,71 +2,86 @@
 
 class Fahrzeug {
 
-    // Datenkapselung = Alle Eigenschaften sind private 
+    // Datenkapselung = Alle Eigenschaften sind private/protected
 
     // Eigenschaften/Attribute = Beschreiben das Objekt / Sind der Zustand des Objekts
     // private, protected, public
     private $kennzeichen; // Instanzeigenschaft
 
-    private $baujahr; // Instanzeigenschaft
+    private $baujahr = 1900; // Instanzeigenschaft
 
     private $marke;
 
     private $modell;
 
-    private $typ;
+    // protected = Nur dieses Objekt (und seine Methoden) und erbende Objekte dürfen diese Information lesen
+    protected $typ;
 
+    // private = Nur dieses Objekt (und seine Methoden) darf diese Information lesen
     private $zustand;
 
+    private $preis;
+
+    private $fahrbereit = true;
+
     // Konstante
-    private const TYPEN = ['PKW', 'LKW'];
+    //private const TYPEN = ['PKW', 'LKW'];
     
+    // Funktionen, die zu einer Klasse gehören nennt man Methoden
+    // Instanzmethoden über ein Objekt aufgerufen werden
+
+    // Instanzmethoden gehören dem Objekt
+    // Klassenmethoden gehören dem Bauplan (Klasse) 
     
-    public function getKennzeichen()
+    // Getter: Lieset den internen Zustand aus und gibt ihn an den Aufrufer der Methode
+    public function getKennzeichen(): string 
     {
         // $this = von diesem Objekt
         return $this->kennzeichen;
     }
 
-    public function setKennzeichen($kennzeichen)
+    public function setKennzeichen(string $kennzeichen): void
     {
         // $kennzeichen = Lokale Variable / Methoden-Parameter
         // $this->kennzeichen = Instanzeigenschaft
         // Lokale Variablen und Parameter werden am Ende der Methode entfernt
         // Instanzeigenschaften bleiben im Objekt erhalten
-        $this->kennzeichen = $kennzeichen;
+        // Format für das Kennzeichen könnte mit regulären Ausdrücken geprüft werden
+        $this->kennzeichen = strtoupper(trim($kennzeichen)); // Daten anpassen
     }
      
-    public function getBaujahr() 
+    public function getBaujahr(): int 
     {
         return $this->baujahr;
     }
 
-    public function setBaujahr($baujahr)
+    public function setBaujahr(int $baujahr): void
     {
         if($baujahr > 1900) {
             $this->baujahr = $baujahr;
         }
     }
 
-    public function getMarke()
+    public function getMarke() : string
     {
         return $this->marke;
     }
 
-    public function setMarke($marke)
+    public function setMarke(string $marke): Fahrzeug
     {
         $this->marke = $marke;
+        return $this; // das $this ist vom Typo Fahrzeug
     }
 
-    public function getModell()
+    public function getModell(): string
     {
         return $this->modell;
     }
 
-    public function setModell($modell)
+    public function setModell(string $modell): Fahrzeug
     {
         $this->modell = $modell;
+        return $this;
     }
 
     public function getZustand()
@@ -86,14 +101,39 @@ class Fahrzeug {
         echo '</pre>';
     }
 
-    public function getTyp()
+    // public function getTyp()
+    // {
+    //     return $this->typ;
+    // }
+
+    // public function setTyp($nr)
+    // {
+    //     // Zugriff auf Konstanten über self
+    //     $this->typ = self::TYPEN[$nr];
+    // }
+
+    public function isFahrbereit(): bool
     {
-        return $this->typ;
+        return $this->fahrbereit;
     }
 
-    public function setTyp($nr)
+    public function getPreisNetto()
     {
-        // Zugriff auf Konstanten über self
-        $this->typ = self::TYPEN[$nr];
+        return $this->preis;
+    }
+
+    public function setPreisNetto($preis)
+    {
+        $this->preis = $preis;
+    }
+
+    public function getPreisBrutto()
+    {
+        return $this->preis * 1.19;
+    }
+
+    public function setPreisBrutto($preis)
+    {
+        $this->preis = ($preis / 119) * 100;
     }
 }
--- oop3.php
+++ oop3.php
@@ -8,10 +8,32 @@ $f1 = new Fahrzeug();
 // Klasse = Bauplan
 // Objekt = Ein konkreter Vertreter einer Klasse
 
-$f1->setBaujahr(1820);
+$f1->setBaujahr(1992);
 $f1->setKennzeichen('HH:AB123');
 
 $f1->printInfo();
 
 echo $f1->getKennzeichen().'<br>';
 echo $f1->getBaujahr().'<br>';
+
+//$f2 = $f1; // Eine neue Referenz auf das gleich Objekt
+
+$f2 = new Fahrzeug();
+//$f2->kennzeichen = 'adsjkdasgdksa'; // ist privat
+$f2->setKennzeichen('   xy:Ac22  '); // Liefert void (Nichts) zurück
+$f2->setMarke('Renault');
+$f2->setModell('Zoe');
+if($f2->isFahrbereit()) {
+    echo 'Ist fahrbereit.<br>';
+}
+
+// Auch möglich, das die Stetter $this zurückgeben (Object-Chaining)
+$f2->setMarke('Renault')->setModell('Zoe');
+
+
+function showKennzeichen($fahrzeug) {
+    echo '<h2>'.$fahrzeug->getKennzeichen().'</h2>';
+}
+
+showKennzeichen($f1);
+showKennzeichen($f2);
