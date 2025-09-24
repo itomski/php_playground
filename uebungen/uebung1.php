1219a596226dd0358b505f01f79bb0961592d757
tlubowiecki <tomasz@lubowiecki.de>
2025-09-24 13:49:08 +0200

    Tag 3

---
+++ uebungen/uebung1.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+Schreibe einen Controller der auf Basis einer Switschanweisung eine HTML Vorlage
+mit Daten füllt.
+
+Die HTML-Vorlage verfügt über die Bereiche header, main, footer
+Und der Controller gibt abhängig von der Auswahl passenden Inhalt für die Bereich vor.
+*/
