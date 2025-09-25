9c8ee4403571655e74d71b10b4f18ef0846a3afe
tlubowiecki <tomasz@lubowiecki.de>
2025-09-25 13:49:06 +0200

    Tag 4

---
+++ uebungen/uebung2.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+Schreibe ein 2d Array welches eine Gästeliste definiert.
+Für jeden Gast in der Liste werden Vorname, Nachname, Firma und Position erfasst.
+Das Array soll mind 5 Personen enthalten.
+
+Schreibe als nächstes eine Funktion, die das Array zu einer HTML-Tabelle verarbeitet.
+Die Tabelle soll auch über Scpaltenbeschreibungen verfügen.
+*/
