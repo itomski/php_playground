172abd14bf6bebeb30218225b8d9ca5ae42ba092
tlubowiecki <tomasz@lubowiecki.de>
2025-09-26 11:37:17 +0200

    Tag 5

---
+++ uebungen/uebung3.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+Schreibe ein Programm für die Verwaltung einer Fahrzeugliste.
+Es besteht aus einem Backend und einem Frontend.
+Das Backend erzeugt ein Array und speichert es in einer Datei.
+
+Über ein Formual soll es möglich sein neue Fahrzeuge zu erfassen. Diese erweitern das Array.
+
+Jedes Fahrzeug besteht auf folgenden Daten: Marke, Modell, Baujahr, Kennzeichen, fahrbereit (bool).
+
+Dazu muss das Array zuerst aus der Datei eingelesen werden, als Array rekonstruiert und dann erweitert.
+Anschließend wird das Array wieder gespeichert.
+
+Das Frontend lest die Daten ein, rekonstruiert sie und zeigt sie in Form einer Tabelle an.
+
+*/
