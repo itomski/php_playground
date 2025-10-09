2f86f8461d70ff629e26ea94d2ac8ffd82f03b62
tlubowiecki <tomasz@lubowiecki.de>
2025-10-09 14:13:21 +0200

    Tag 13

---
+++ uebungen/uebung8.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+Baue ein Formular zum Erfassen von Filmen. Es soll den Titel, Beschreibung, Länge und Verleih speichern können.
+
+Schreibe als nächstes eine Klasse die einen Film repräsentiert. 
+Der Film wird nach dem absenden des Formulars aus den verschickten Daten erstellt und in einer Sammlung abgelegt.
+Die Sammlung soll in einer Datei abgelegt werden.
+
+Das Film Objekt soll direkt aus dem POST-Array "hydriert" werden.
+
+Baue eine Möglichkeit auch alle Filme in der Sammlung anzuzeigen.
+
+*/
